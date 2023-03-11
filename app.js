c6a4fb6dec563d5fc96aacfbebd8382b6d9e6e2d
Josiah Jacobs <joshuajacobs87@gmail.com>
2023-03-11 12:27:22 +0200

    nav , css,

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("gallery");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
+}
