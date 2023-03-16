81a300df41df13a3499f4d1710dabe6a05d1e6c4
Josiah Jacobs <joshuajacobs87@gmail.com>
2023-03-16 18:43:08 +0200

    showing gallery page with animation

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
+
+
 function myFunction() {
   var x = document.getElementById("gallery");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+  x.classList.add("visible");
 }
