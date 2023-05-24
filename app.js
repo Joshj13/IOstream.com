05f974340d4a0383beedabbeff765fb4c2a1c777
Josiah Jacobs <joshuajacobs87@gmail.com>
2023-05-24 11:33:51 +0200

    removed the unnecessary closing div tags and adjusted the indentation for better readability.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function myFunction() {
+  console.log("Button clicked!"); // Add this line to see if the function is being executed
   var x = document.getElementById("gallery");
   if (x.classList.contains("visible")) {
     x.classList.remove("visible");
